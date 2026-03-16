6266f874f242a59cd9904d03351aed41af0a170b
Berke Yilmaz <berkeyilmaz189@gmail.com>
2026-03-16 21:09:16 +0300

    standartize(padding n cropping) added

--- src/augment.py
+++ src/augment.py
@@ -1,6 +1,16 @@
 import librosa
 import config  as CONFIG
+import numpy as np
 
 def load_audio(path):
     signal, sr = librosa.load(path, sr=CONFIG.SAMPLE_RATE)
     return signal, sr
+
+def standardize(signal):
+    """Padding veya cropping ile sabit uzunluğa getirir."""
+    target = int(CONFIG.SAMPLE_RATE * CONFIG.TARGET_DURATION)
+    if len(signal) < target:
+        signal = np.pad(signal, (0, target - len(signal)))
+    else:
+        signal = signal[:target]
+    return signal
