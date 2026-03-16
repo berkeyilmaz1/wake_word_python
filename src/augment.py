de73233497cc98f7eed5efc58c328ffa0c155fbf
Berke Yilmaz <berkeyilmaz189@gmail.com>
2026-03-16 21:20:01 +0300

    augment_file fun added

--- src/augment.py
+++ src/augment.py
@@ -1,6 +1,8 @@
 import librosa
 import config  as CONFIG
 import numpy as np
+import os
+import soundfile as sf 
 
 def load_audio(path):
     signal, sr = librosa.load(path, sr=CONFIG.SAMPLE_RATE)
@@ -26,3 +28,27 @@ def change_pitch(signal, sr):
 def change_speed(signal):
     rate = np.random.uniform(*CONFIG.SPEED_RANGE)
     return librosa.effects.time_stretch(signal, rate)
+
+def augment_file(file_path,output_path,n_copies):
+    signal, sr = load_audio(file_path)
+    signal = standardize(signal)
+
+    base_name = os.path.splitext(os.path.basename(file_path))[0]
+
+    #Her versiyona 1-3 arası rastgele bir augmentasyon uygula
+    for i in range(n_copies):
+        augmented = signal.copy()
+        ops = np.random.choice(
+            [add_noise,
+             lambda a: change_pitch(a, sr),
+             change_speed],
+            size=np.random.randint(1, 3),
+            replace=False
+        )
+
+        for op in ops:
+            augmented = op(augmented)
+        
+        signal_aug= standardize(augmented)
+        output_file = os.path.join(output_path, f"{base_name}_aug_{i}.wav")
+        sf.write(output_file, signal_aug, sr)
